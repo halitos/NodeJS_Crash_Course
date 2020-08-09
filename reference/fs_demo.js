c6ee5589d6762538f91e2ceabc71e7c961aa596e
Halit <60817499+halitos@users.noreply.github.com>
2020-08-09 21:35:01 +0100

    practice fs/os modules

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -8,11 +8,37 @@ const path = require("path");
 // });
 
 // Create and write file
-fs.writeFile(
+// fs.writeFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   "hello this is content",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("file created and written");
+
+//     // file append
+//     fs.appendFile(
+//       path.join(__dirname, "/test", "hello.txt"),
+//       " for node.js",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("file created and written");
+//       }
+//     );
+//   }
+// );
+
+//-----Read File-------
+// fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+//----Rename file---------
+fs.rename(
   path.join(__dirname, "/test", "hello.txt"),
-  "hello this is content",
+  path.join(__dirname, "/test", "hola.txt"),
   (err) => {
     if (err) throw err;
-    console.log("file created and written");
+    console.log("file renamed");
   }
 );
---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require("os");
+
+//---platform------
+console.log(os.platform());
+
+//----CPU Arch
+console.log(os.arch());
+
+//-----CPU Core info----
+console.log(os.cpus());
+
+//---Free Memory---
+console.log(os.freemem());
+
+//---Toal Memory---
+console.log(os.totalmem());
+
+//-----Hom eDir----
+console.log(os.homedir());
+
+//-----Uptime-----
+console.log(os.uptime());
