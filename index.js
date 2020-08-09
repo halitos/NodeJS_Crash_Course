78afbd67a295b801f625d2dcddd4b5dcf86b9aa8
Halit <60817499+halitos@users.noreply.github.com>
2020-08-10 00:13:54 +0100

    http req started

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const Person = require("./person");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const person1 = new Person("halit", 41);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("<h1>home</h1>");
+  }
+});
 
-person1.greeting();
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
