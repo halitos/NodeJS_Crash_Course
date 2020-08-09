60868967f719f385cc54cbe330e4bc561a3e8976
Halit <60817499+halitos@users.noreply.github.com>
2020-08-09 23:06:05 +0100

    url modules added

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -1,5 +1,34 @@
 const url = require("url");
 
-const myUrl = new URL("http://mywebsite.com/hello.html?id=100&status=active");
+const myUrl = new URL(
+  "http://mywebsite.com:8080/hello.html?id=100&status=active"
+);
 
 //-----Serialised URL----
+console.log(myUrl.href);
+console.log(myUrl.toString()); //same
+
+//-----Host (root domain)----
+console.log(myUrl.host);
+
+//-----Hostname-------
+console.log(myUrl.hostname); //does not incl "port"
+
+//-----port------
+console.log(myUrl.port);
+
+//-----Pathname-----
+console.log(myUrl.pathname);
+
+//----serialised Query---
+console.log(myUrl.search);
+
+//-----search Params Object------
+console.log(myUrl.searchParams);
+
+//----Add Param------
+myUrl.searchParams.append("limit", 3);
+console.log(myUrl.searchParams);
+
+//----loop through Params----
+myUrl.searchParams.forEach((value, name) => console.log(name, ":", value));
